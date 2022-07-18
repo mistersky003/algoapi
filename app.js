9d3a729031b12fdfe31d3d58e8405ae837f6ba4a
Denys <d.dzhugalyk@nltu.lviv.ua>
2022-07-18 15:10:40 +0300

    changed app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', routes)
 app.use(function(req,res) {
-    res.send({status: 400, massege: "Bad Request"});
+    res.status(400).send({status: 400, massege: "Bad Request"});
 })
 
 app.listen(process.env.PORT, port, () =>
